de138428df0979961c77b3cd339183c7c21d84ae
pangil5634 <oksk6681@naver.com>
2024-04-03 23:03:48 +0900

    add : seminar 3 files

--- pard_3rd_seminar_3/src/App.js
+++ pard_3rd_seminar_3/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Route, Routes} from 'react-router-dom';
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from './Pages/Profile';
+import Articles from './Pages/Articles';
+import Article from './Pages/Article';
+import Layout from './Pages/Layout';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route element={<Layout/>}>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/profiles/:username" element={<Profile />}/>
+                <Route path="/articles" element={<Articles />}>
+                    <Route path=":id" element={<Article />}/>
+                </Route>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
+
+
+
